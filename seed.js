9bc28436e235fe2cb1be47fa5f3f478fd5df3955
tiffanyteaze <tteaze@gmail.com>
2019-03-02 13:09:37 -0800

    adds model for videogames and seeds it

--- seed.js
+++ seed.js
@@ -1,7 +1,7 @@
 // This file allows us to seed our application with data
 // simply run: `node seed.js` from the root of this project folder.
 
-// const db = require('./models');
+const db = require('./models');
 
 // const new_campsite = {description: "Sharp rocks. Middle of nowhere."}
 
@@ -9,6 +9,19 @@
 //   if (err){
 //     return console.log("Error:", err);
 //   }
+
+const new_game = {
+    title: "Majora's Mask",
+    avatar: "Link"
+}
+
+db.Game.create(new_game, function(err, game){
+    if (err){
+        return console.log("Error:", err);
+    }
+    console.log("Created new game entry", game._id)
+    process.exit();
+})
 
 //   console.log("Created new campsite", campsite._id)
 //   process.exit(); // we're all done! Exit the program.
