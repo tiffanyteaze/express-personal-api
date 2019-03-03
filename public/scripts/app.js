c810e4dbce955474b9258929489d7daf938e3bb0
tiffanyteaze <tteaze@gmail.com>
2019-03-02 21:02:04 -0800

    adds create new game functionality to frontend submit form

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,30 +1,67 @@
 console.log("Sanity Check: JS is working!");
+let allGames = [];
+let $gameList;
 
 $(document).ready(function(){
+    $gameList = $('#game-list');
 
-    function handleSuccess(game) {
+    //list all games on index page
+    function gameListSuccess(game) {
         for (i = 0; i < game.length; i++) {
-            $('#listview').append( `<li> Game: ${game[i].title}, Avatar: ${game[i].avatar}</li>`);
+            $('#game-list').append( `<li> Game: ${game[i].title}, Avatar: ${game[i].avatar}</li>`);
         }
     }
 
-    function  handleError() {
+    function gameListError() {
         console.log('something went wrong');
-        $('#listview').text('Failed to load books, is the server working?');
+        $('#game-list').text('Failed to load books, is the server working?');
     }
 
-    //Read all games
     $.ajax({
         method: 'GET',
         url: '/api/games',
-        success: handleSuccess,
-        error: handleError
-    })
+        success: gameListSuccess,
+        error: gameListError
+    });
+
+    //HTML to append when new game is added
+    function getGameHtml(game) {
+        return `<p>${game.title}, ${game.avatar}</p>`;
+    }
+
+    //maps through array of games and returns it as a string
+    function getAllGamesHtml(games) {
+        return games.map(getGameHtml).join("");
+    }
+
+    //renders all new games to index page
+    function render () {
+        $gameList.empty();
+        let gameHtml = getAllGamesHtml(allGames);
+        $gameList.append(gameHtml);
+    }
 
     //create a new game
-    $(`form`).submit(function(e){
+    function newGameSuccess(json) {
+        $('#game-form input').val('');
+        allGames.push(json);
+        render();
+    }
+
+    function newGameError() {
+        console.log('There was an error with adding a new game.');
+    }
+
+    $('#game-form').on('submit', function(e){
         e.preventDefault();
-        console.log("you clicked submit");
-    })
+        console.log('you clicked');
+        $.ajax({
+            method: 'POST',
+            url: '/api/games',
+            data: $(this).serialize(),
+            success: newGameSuccess,
+            error: newGameError
+        });
+    });
 
 });
