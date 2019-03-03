75a6cbefe216a50514ecf15db9338907f58b05c5
tiffanyteaze <tteaze@gmail.com>
2019-03-03 11:14:05 -0800

    adds delete functionality to front end

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -6,17 +6,6 @@ $(document).ready(function(){
     $gameList = $('#game-list');
 
     //list all games on index page
-    function gameListSuccess(game) {
-        for (i = 0; i < game.length; i++) {
-            $('#game-list').append( `<li> Game: ${game[i].title}, Avatar: ${game[i].avatar}</li>`);
-        }
-    }
-
-    function gameListError() {
-        console.log('something went wrong');
-        $('#game-list').text('Failed to load books, is the server working?');
-    }
-
     $.ajax({
         method: 'GET',
         url: '/api/games',
@@ -26,7 +15,15 @@ $(document).ready(function(){
 
     //HTML to append when new game is added
     function getGameHtml(game) {
-        return `<p>${game.title}, ${game.avatar}</p>`;
+        return `<p>
+            ${game.title}, ${game.avatar}
+            <button 
+                type="delete" 
+                class="deleteBtn"
+                data-id=${game._id}>
+                Delete
+            </button>
+         </p>`;
     }
 
     //maps through array of games and returns it as a string
@@ -34,11 +31,22 @@ $(document).ready(function(){
         return games.map(getGameHtml).join("");
     }
 
-    //renders all new games to index page
+    //renders all games to index page
     function render () {
         $gameList.empty();
         let gameHtml = getAllGamesHtml(allGames);
         $gameList.append(gameHtml);
+    }
+
+    //lists all games on index page
+    function gameListSuccess(games) {
+        allGames = games;
+        render();
+    }
+
+    function gameListError() {
+        console.log('something went wrong');
+        $('#game-list').text('Failed to load books, is the server working?');
     }
 
     //create a new game
@@ -52,6 +60,7 @@ $(document).ready(function(){
         console.log('There was an error with adding a new game.');
     }
 
+    //event listener for new game submit form
     $('#game-form').on('submit', function(e){
         e.preventDefault();
         console.log('you clicked');
@@ -63,5 +72,31 @@ $(document).ready(function(){
             error: newGameError
         });
     });
+    //delete a games
+    function deleteGameSuccess(json) {
+        let game = json;
+        let gameId = game._id;
+        for (i = 0; i < allGames.length; i++){
+            if (allGames[i]._id === gameId) {
+                allGames.splice(i, 1);
+                break;
+            }
+        }
+        render();
+    }
+
+    function deleteGameError() {
+        console.log("deleting game was not successful")
+    }
+
+    //event listener for delete game button
+    $gameList.on('click', '.deleteBtn', function() {
+        $.ajax({
+            method: 'DELETE',
+            url: 'api/games/'+$(this).attr('data-id'),
+            success: deleteGameSuccess,
+            error: deleteGameError
+        })
+    })
 
 });
