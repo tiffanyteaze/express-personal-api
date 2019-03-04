eb1766626fb8921783f78fc72d78700b47d02710
tiffanyteaze <tteaze@gmail.com>
2019-03-03 17:45:10 -0800

    removes redundant tiffany-gamerlog repository

--- tiffany-gamerlog/models/index.js
+++
@@ -1,4 +0,0 @@
-const mongoose = require("mongoose");
-mongoose.connect( process.env.MONGODB_URI || "https://git.heroku.com/tiffany-gamerlog.git", {useMongoClient: true});
-
-// module.exports.Campsite = require("./campsite.js.example");
